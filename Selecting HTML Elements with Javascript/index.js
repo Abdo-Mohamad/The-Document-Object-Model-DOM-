3727171ee4b8ee0ee82015daadd493e2096421f4
Abdo Mohamad <abdomohamadwalid@gmail.com.com>
2024-06-21 14:46:54 +0200

    Selecting HTML Elements with Javascript

---
+++ Selecting HTML Elements with Javascript/index.js
@@ -0,0 +1,8 @@
+//My Solution 
+
+let changeColorRed = document.querySelector(".list a");
+changeColorRed.style.color = "red";
+
+
+// Angela Solution 
+document.querySelector("li a").style.color = "blue";
