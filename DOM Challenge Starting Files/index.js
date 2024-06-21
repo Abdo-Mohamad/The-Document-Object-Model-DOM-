7847ba2968a34f27eed2bc1488e3d626e9b9bfd9
Abdo Mohamad <abdomohamadwalid@gmail.com.com>
2024-06-21 14:31:42 +0200

    Introduction to the Document Object Model (DOM)

---
+++ DOM Challenge Starting Files/index.js
@@ -0,0 +1,8 @@
+//My Solution 
+
+let test = document.querySelector(".list:last-child");
+test.innerHTML = "Abdo";
+
+
+// Angela Solution 
+document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Angela";
