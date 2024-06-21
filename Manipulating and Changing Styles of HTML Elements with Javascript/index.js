0a367c8a712b6400ceb65141e1f2e98e41c26389
Abdo Mohamad <abdomohamadwalid@gmail.com.com>
2024-06-21 14:54:33 +0200

    Selecting HTML Elements with Javascript

---
+++ Manipulating and Changing Styles of HTML Elements with Javascript/index.js
@@ -0,0 +1,7 @@
+//My Solution 
+//Manipulating and Changing Styles of HTML Elements with Javascript
+document.querySelector("button").style.backgroundColor = " yellow ";
+
+
+// Angela Solution 
+document.querySelector("button").style.backgroundColor = " yellow ";
