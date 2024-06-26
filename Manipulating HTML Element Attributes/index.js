2115e6172b21c9418081f5a131e95becfa17f1fd
Abdo Mohamad <abdomohamadwalid@gmail.com.com>
2024-06-26 15:50:18 +0200

    Manipulating HTML Element Attributes

---
+++ Manipulating HTML Element Attributes/index.js
@@ -0,0 +1,5 @@
+//My Solution 
+
+document.querySelector("a").innerHTML = ("Bing");
+document.querySelector("a").getAttribute("href");
+document.querySelector("a").setAttribute("href", "https://www,.bing.com");
